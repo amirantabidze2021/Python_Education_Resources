fb1d60cd03f837e09019ce5a0877ef5cad854dcd
Amiran Tabidze <81627817+amirantabidze2021@users.noreply.github.com>
2024-02-26 08:54:32 +0400

    _First

---
+++ _3_lesson.py
@@ -0,0 +1,93 @@
+
+# new line in print function
+
+# print("Hello\nWorld\n Computer")   # \n  new line
+
+
+# escape in print function
+
+# print("Hello\"World\Computer") 
+
+# end line in print function
+
+#print("Hello World" , end = "")
+
+# sep parameter in print function
+
+#print("HelloWorld","Computer" ,"Python", sep = '@')
+
+
+# f- format in print function 
+# sigrdze = 10 
+# sigane = 5 
+# fartobi = sigrdze * sigane
+# #print("fartobi = ", fartobi)
+# #print("sigane = " , sigane , "sigrdze=  " , sigrdze , "fartobi = " , fartobi)
+# print(f"sigane = {sigane}, sigrdze = {sigrdze} , fartobi = {fartobi}")   # ფორმატირების წესი f -ით
+
+
+# string concatenation -შეკრება
+
+# brand = " HP "
+# model = "elitebook "
+# raodenoba = 3 
+# print(brand * raodenoba)   # string-ის და Int-ის დაკავშირება ერთმანეთთან
+# print(brand + model + str(raodenoba))
+# print(brand, model)
+
+
+#mathematical operation  and operators
+
+# a = 15.5 
+# b = 5 
+# c = a + b  # შეკრება
+# d = a - b  # გამოკლება
+# e = a * b  #გამრავლება 
+# f = a / b  # გაყოფა
+# g = a**7  # ახარისხება 
+# h = a // b  # // -ეს გაყოფა არის მთელი რიცხვის მოცემისთვის 
+# m = a % b   # % არის ნაშთის მოსაცემად
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+# print(g)
+# print(h)
+# print(m)
+
+# # შედარების ოპერატორები  - Comparison operators
+
+# print(a == b)  # "==" ეს არის მათემატიკური ტოლობა
+# print( a != b ) # "!=" არ არის ტოლი
+# print( a > b)
+# print( a < b ) 
+# print( a >= b)  # მეტია ან ტოლია
+# print( a <= b )   # ნაკლებია ან ტოლია
+
+#Conditional Statement  -პირობითი განაცხადები
+
+# x = 5
+# if x > 10:
+#     print('Smaller')      # მთავარია if -ის პირობა იყოს True    
+#                             # თუ if -ის პირობა False-ია მაშინ არ სესრულდება
+# if x < 20:
+#     print('Bigger')
+
+# print('Finish')
+
+
+# age = 17 
+
+# if age > 20 :   #false
+#     print ("ის სრულწლოვანია")
+#     if age == 17:  # true
+#         print("is aris 20 -is ")
+#         if age >= 15 :  #true
+#             print()
+
+# elif age < 18 :
+#     print("ის შეიძლება იყოს სრულწლოვანი")
+    
+# else:
+#     print("ის თინეიჯერია")
+    
