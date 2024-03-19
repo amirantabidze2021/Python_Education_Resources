8611d7d79cfae166465a5c480b2ac4dfc9d4a79f
Amiran Tabidze <81627817+amirantabidze2021@users.noreply.github.com>
2024-03-19 10:45:38 +0400

    Add files via upload

---
+++ _2_oop_lesson.py
@@ -0,0 +1,29 @@
+#abstraction
+
+# encapsulation methods is 1 ) getter ()  2 ) setter() 
+
+# getter () მეთოდი აბრუნებს ატრიბუტების მნიშვნელობებს 
+
+# setter () მეთოდი ცვლის ატრიბუტის მნიშვნელობებს .
+
+# class kids :
+#     def __init__(self, firstname, surname):
+#         self.firstname = firstname 
+#         self.surname = surname 
+
+#     def get_name(self):
+#         return self.firstname
+    
+#     def set_name(self, text):
+#         self.firstname = text 
+
+# pupils1 = kids("Amiran", "Tabidze")      #object pupils1 , kids არის კლასი
+
+# #print(pupils1.firstname) #- არ არის რეკომენდირებული
+# #print(pupils1.get_name())   # getter მეთოდი
+# pupils1.set_name = "Nika"
+# print(pupils1.set_name)    
+
+######################################################################################
+
+
